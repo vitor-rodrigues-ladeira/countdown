9395cb90751ab0095ca5ebeceb5a712202b89cde
vitor-rodrigues-ladeira <vitor.rodrigues@ladeirabijuterias.com.br>
2026-05-26 12:08:04 -0300

    feat: schema do target date

--- store-block-template/react/Countdown.tsx
+++ store-block-template/react/Countdown.tsx
@@ -14,7 +14,14 @@ Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador.',
+      type: 'string',
+      default: null
+    }
+  },
 }
 
 export default Countdown
